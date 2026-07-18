e75317428b110a3da75a7b6fc7bd5acd499f3bf3
BasavarajBhaire <basavarajbhaire@gmail.com>
2026-07-18 16:25:50 +0530

    Add Educational Compilation Engine package

---
+++ eduos/compiler/__init__.py
@@ -0,0 +1,5 @@
+"""BCube Educational Compilation Engine (ECE)."""
+
+from .compiler import CompilationError, CompilationResult, EducationalCompiler
+
+__all__ = ["CompilationError", "CompilationResult", "EducationalCompiler"]
